49948132f512c1cfb3ac93c600650b894776cbae
Adel Sungatullin <128482853+lutagnus@users.noreply.github.com>
2024-08-19 14:13:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -21,13 +21,14 @@ app.include_router(router)
 
 origins = [
     "https://fs-rnrmu.ru:443",
+    "https://fs-rnrmu.ru",
 ]
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["GET", "POST", "OPTIONS", "DELETE", "PATCH", "PUT"],
+    allow_methods=["GET", "POST", "DELETE", "PUT"],
     allow_headers=["Content-Type", "Set-Cookie", "Access-Control-Allow-Headers", "Access-Control-Allow-Origin",
                    "Authorization"],
 )
