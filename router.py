2cf13dea155ba8732994e9e2bcf5ccd9730db8e7
Adel Sungatullin <128482853+lutagnus@users.noreply.github.com>
2024-08-26 14:19:20 +0300

    Update router.py

--- router.py
+++ router.py
@@ -42,7 +42,7 @@ async def delete_event(event_id: int):
 
 @router.post("/api/events/{event_id}/register", response_model=STask)
 async def register_for_event(event_id: int, user_id: str):
-    events = await TaskRepository.get_tasks()
+    events = await TaskRepository.update_task()
     event = next((event for event in events if event.id == event_id), None)
     if event is None:
         raise HTTPException(status_code=404, detail="Event not found")
