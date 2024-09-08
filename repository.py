3e54b5b3b9af4c4bba805c9fb98e1269cfa2e313
Adel Sungatullin <128482853+lutagnus@users.noreply.github.com>
2024-09-08 21:54:58 +0300

    Update repository.py

--- repository.py
+++ repository.py
@@ -62,6 +62,14 @@ class TaskRepository:
                 select(EventOrm).where(EventOrm.participants.contains([str(user_id)]))
             )
             return events.scalars().all()
+
+   @staticmethod
+   async def get_user_by_id(user_id: str):
+        async with new_session() as session:
+            user = await session.get(UserOrm, user_id)
+            if not user:
+                raise ValueError(f"User with id {user_id} not found")
+            return user
    
    @staticmethod
    async def get_users():
